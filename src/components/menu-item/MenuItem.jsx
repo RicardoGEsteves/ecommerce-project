a457c93b8046afda0ff261aac6ce955f9e3e0d6b
Ricardo Esteves <you@example.com>
2020-01-25 16:50:48 +0000

    finish slyting homepage and menu-item components

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -4,12 +4,13 @@ import './menu-item.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`menu-item ${size}`}
-    >
+    <div className={`menu-item ${size}`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
       </div>
     </div>
