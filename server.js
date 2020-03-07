a88d68f8fa4d7e0a8d6acbe6fe059136f6d591b3
Ricardo Esteves <you@example.com>
2020-03-07 02:52:58 +0000

    changed currency for stripe on server.js

--- server.js
+++ server.js
@@ -1,48 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-
-// if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(cors());
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-//   );
-// }
-
-// app.listen(port, error => {
-//   if (error) throw error;
-
-//   console.log(`server running on port ${port}`);
-// });
-
-// app.post('/payment', (req, res) => {
-//   const body = {
-//     source: req.body.token.id,
-//     amount: req.body.amount,
-//     currency: 'EUR'
-//   };
-
-//   stripe.charges.create(body, (stripeErr, stripeRes) => {
-//     stripeErr
-//       ? res.status(500).send({ error: stripeErr })
-//       : res.status(200).send({ success: stripeRes });
-//   });
-// });
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -77,7 +32,7 @@ app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd'
+    currency: 'EUR'
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
